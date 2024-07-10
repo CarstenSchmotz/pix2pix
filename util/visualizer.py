bfa31a1bd5e4283ca52c5ac29c6ac9aa4de764d2
larrystahl <131356333+CarstenSchmotz@users.noreply.github.com>
2024-07-10 16:16:27 +0200

    new visualser

--- util/visualizer.py
+++ util/visualizer.py
@@ -113,4 +113,122 @@ class Visualizer():
                 idx = 0
                 for label, image in visuals.items():
                     image_numpy = util.tensor2im(image)
-         
+                    if label == 'input_rgb_depth':  # Handle combined input separately
+                        rgb_numpy = image_numpy[:, :, :3]
+                        depth_numpy = image_numpy[:, :, 3]
+                        depth_numpy = np.expand_dims(depth_numpy, axis=2).repeat(3, axis=2)
+                        image_numpy = np.concatenate((rgb_numpy, depth_numpy), axis=1)
+                    label_html_row += '<td>%s</td>' % label
+                    images.append(image_numpy.transpose([2, 0, 1]))
+                    idx += 1
+                    if idx % ncols == 0:
+                        label_html += '<tr>%s</tr>' % label_html_row
+                        label_html_row = ''
+                white_image = np.ones_like(image_numpy.transpose([2, 0, 1])) * 255
+                while idx % ncols != 0:
+                    images.append(white_image)
+                    label_html_row += '<td></td>'
+                    idx += 1
+                if label_html_row != '':
+                    label_html += '<tr>%s</tr>' % label_html_row
+                try:
+                    self.vis.images(images, nrow=ncols, win=self.display_id + 1,
+                                    padding=2, opts=dict(title=title + ' images'))
+                    label_html = '<table>%s</table>' % label_html
+                    self.vis.text(table_css + label_html, win=self.display_id + 2,
+                                  opts=dict(title=title + ' labels'))
+                except VisdomExceptionBase:
+                    self.create_visdom_connections()
+
+            else:     # show each image in a separate visdom panel;
+                idx = 1
+                try:
+                    for label, image in visuals.items():
+                        image_numpy = util.tensor2im(image)
+                        if label == 'input_rgb_depth':  # Handle combined input separately
+                            rgb_numpy = image_numpy[:, :, :3]
+                            depth_numpy = image_numpy[:, :, 3]
+                            depth_numpy = np.expand_dims(depth_numpy, axis=2).repeat(3, axis=2)
+                            image_numpy = np.concatenate((rgb_numpy, depth_numpy), axis=1)
+                        self.vis.image(image_numpy.transpose([2, 0, 1]), opts=dict(title=label),
+                                       win=self.display_id + idx)
+                        idx += 1
+                except VisdomExceptionBase:
+                    self.create_visdom_connections()
+
+        if self.use_wandb:
+            columns = [key for key, _ in visuals.items()]
+            columns.insert(0, 'epoch')
+            result_table = wandb.Table(columns=columns)
+            table_row = [epoch]
+            ims_dict = {}
+            for label, image in visuals.items():
+                image_numpy = util.tensor2im(image)
+                if label == 'input_rgb_depth':  # Handle combined input separately
+                    rgb_numpy = image_numpy[:, :, :3]
+                    depth_numpy = image_numpy[:, :, 3]
+                    depth_numpy = np.expand_dims(depth_numpy, axis=2).repeat(3, axis=2)
+                    image_numpy = np.concatenate((rgb_numpy, depth_numpy), axis=1)
+                wandb_image = wandb.Image(image_numpy)
+                table_row.append(wandb_image)
+                ims_dict[label] = wandb_image
+            self.wandb_run.log(ims_dict)
+            if epoch != self.current_epoch:
+                self.current_epoch = epoch
+                result_table.add_data(*table_row)
+                self.wandb_run.log({"Result": result_table})
+
+        if self.use_html and (save_result or not self.saved):  # save images to an HTML file if they haven't been saved.
+            self.saved = True
+            # save images to the disk
+            for label, image in visuals.items():
+                image_numpy = util.tensor2im(image)
+                if label == 'input_rgb_depth':  # Handle combined input separately
+                    rgb_numpy = image_numpy[:, :, :3]
+                    depth_numpy = image_numpy[:, :, 3]
+                    depth_numpy = np.expand_dims(depth_numpy, axis=2).repeat(3, axis=2)
+                    image_numpy = np.concatenate((rgb_numpy, depth_numpy), axis=1)
+                img_path = os.path.join(self.img_dir, 'epoch%.3d_%s.png' % (epoch, label))
+                util.save_image(image_numpy, img_path)
+
+            # update website
+            webpage = html.HTML(self.web_dir, 'Experiment name = %s' % self.name, refresh=1)
+            for n in range(epoch + 1):
+                webpage.add_header('epoch [%d]' % n)
+                ims, txts, links = [], [], []
+                for label, image_numpy in visuals.items():
+                    img_path = 'epoch%.3d_%s.png' % (n, label)
+                    ims.append(img_path)
+                    txts.append(label)
+                    links.append(img_path)
+                webpage.add_images(ims, txts, links, width=self.win_size)
+            webpage.save()
+
+    def plot_current_losses(self, epoch, counter_ratio, losses):
+        """display the current losses on visdom display: dictionary of error labels and values"""
+        if not hasattr(self, 'plot_data'):
+            self.plot_data = {'X': [], 'Y': [], 'legend': list(losses.keys())}
+        self.plot_data['X'].append(epoch + counter_ratio)
+        self.plot_data['Y'].append([losses[k] for k in self.plot_data['legend']])
+        try:
+            self.vis.line(
+                X=np.stack([np.array(self.plot_data['X'])] * len(self.plot_data['legend']), 1),
+                Y=np.array(self.plot_data['Y']),
+                opts={
+                    'title': self.name + ' loss over time',
+                    'legend': self.plot_data['legend'],
+                    'xlabel': 'epoch',
+                    'ylabel': 'loss'},
+                win=self.display_id)
+        except VisdomExceptionBase:
+            self.create_visdom_connections()
+
+    def print_current_losses(self, epoch, iters, losses, t_comp, t_data):
+        """print current losses on console; also save the losses to the disk"""
+        message = '(epoch: %d, iters: %d, time: %.3f, data: %.3f) ' % (epoch, iters, t_comp, t_data)
+        for k, v in losses.items():
+            message += '%s: %.3f ' % (k, v)
+
+        print(message)  # print the message
+        with open(self.log_name, "a") as log_file:
+            log_file.write('%s\n' % message)  # save the message
