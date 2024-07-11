adf0dbc0941f4a01b6a0bae44e44fc2c77edc775
larrystahl <131356333+CarstenSchmotz@users.noreply.github.com>
2024-07-11 11:33:31 +0200

    Update visualizer.py

--- util/visualizer.py
+++ util/visualizer.py
@@ -18,7 +18,17 @@ else:
     VisdomExceptionBase = ConnectionError
 
 def save_images(webpage, visuals, image_path, aspect_ratio=1.0, width=256, use_wandb=False):
-    """Save images to the disk."""
+    """Save images to the disk.
+
+    Parameters:
+        webpage (the HTML class) -- the HTML webpage class that stores these images (see html.py for more details)
+        visuals (OrderedDict)    -- an ordered dictionary that stores (name, images (either tensor or numpy) ) pairs
+        image_path (str)         -- the string is used to create image paths
+        aspect_ratio (float)     -- the aspect ratio of saved images
+        width (int)              -- the images will be resized to width x width
+
+    This function will save images stored in 'visuals' to the HTML file specified by 'webpage'.
+    """
     image_dir = webpage.get_image_dir()
     short_path = ntpath.basename(image_path[0])
     name = os.path.splitext(short_path)[0]
@@ -28,27 +38,38 @@ def save_images(webpage, visuals, image_path, aspect_ratio=1.0, width=256, use_w
     ims_dict = {}
     for label, im_data in visuals.items():
         im = util.tensor2im(im_data)
-        if label == 'input_rgb_depth':  # Handle combined input separately
-            rgb_numpy = im[:, :, :3]
-            depth_numpy = im[:, :, 3]
-            depth_numpy = np.expand_dims(depth_numpy, axis=2).repeat(3, axis=2)
-            im_rgb = rgb_numpy
-            im_depth = depth_numpy
-            image_name_rgb = '%s_%s_rgb.png' % (name, label)
-            image_name_depth = '%s_%s_depth.png' % (name, label)
-            save_path_rgb = os.path.join(image_dir, image_name_rgb)
-            save_path_depth = os.path.join(image_dir, image_name_depth)
-            util.save_image(im_rgb, save_path_rgb, aspect_ratio=aspect_ratio)
-            util.save_image(im_depth, save_path_depth, aspect_ratio=aspect_ratio)
-            ims.append(image_name_rgb)
-            ims.append(image_name_depth)
-            txts.append(label + '_rgb')
-            txts.append(label + '_depth')
-            links.append(image_name_rgb)
-            links.append(image_name_depth)
-            if use_wandb:
-                ims_dict[label + '_rgb'] = wandb.Image(im_rgb)
-                ims_dict[label + '_depth'] = wandb.Image(im_depth)
+        if label == 'real_A':  # Handle combined input separately
+            # Assuming real_A contains RGB and depth channels concatenated
+            if im.shape[2] == 4:  # Check if there are 4 channels
+                rgb_numpy = im[:, :, :3]
+                depth_numpy = im[:, :, 3]
+                depth_numpy = np.expand_dims(depth_numpy, axis=2).repeat(3, axis=2)  # Convert depth to 3 channels for visualization
+                im_rgb = rgb_numpy
+                im_depth = depth_numpy
+                image_name_rgb = '%s_%s_rgb.png' % (name, label)
+                image_name_depth = '%s_%s_depth.png' % (name, label)
+                save_path_rgb = os.path.join(image_dir, image_name_rgb)
+                save_path_depth = os.path.join(image_dir, image_name_depth)
+                util.save_image(im_rgb, save_path_rgb, aspect_ratio=aspect_ratio)
+                util.save_image(im_depth, save_path_depth, aspect_ratio=aspect_ratio)
+                ims.append(image_name_rgb)
+                ims.append(image_name_depth)
+                txts.append(label + '_rgb')
+                txts.append(label + '_depth')
+                links.append(image_name_rgb)
+                links.append(image_name_depth)
+                if use_wandb:
+                    ims_dict[label + '_rgb'] = wandb.Image(im_rgb)
+                    ims_dict[label + '_depth'] = wandb.Image(im_depth)
+            else:
+                image_name = '%s_%s.png' % (name, label)
+                save_path = os.path.join(image_dir, image_name)
+                util.save_image(im, save_path, aspect_ratio=aspect_ratio)
+                ims.append(image_name)
+                txts.append(label)
+                links.append(image_name)
+                if use_wandb:
+                    ims_dict[label] = wandb.Image(im)
         else:
             image_name = '%s_%s.png' % (name, label)
             save_path = os.path.join(image_dir, image_name)
